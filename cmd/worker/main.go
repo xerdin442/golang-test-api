9f774134fd0bc0926d92b50599493e155c0d4125
xerdin442 <mudianthonio27@gmail.com>
2026-01-15 22:23:23 +0100

    configured scheduler for cron jobs

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/hibiken/asynq"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/rs/zerolog/log"
@@ -9,18 +13,50 @@ import (
 )
 
 func main() {
-	addr := env.GetStr("REDIS_ADDR")
-	password := env.GetStr("REDIS_PASSWORD")
+	redisConn := asynq.RedisClientOpt{
+		Addr:     env.GetStr("REDIS_ADDR"),
+		Password: env.GetStr("REDIS_PASSWORD"),
+	}
 
+	// Initialize the worker server
 	srv := asynq.NewServer(
-		asynq.RedisClientOpt{Addr: addr, Password: password},
+		redisConn,
 		asynq.Config{Concurrency: 10},
 	)
 
+	// Define tasks handlers
 	mux := asynq.NewServeMux()
 	mux.HandleFunc("email_queue", tasks.HandleEmailTask)
 
-	if err := srv.Run(mux); err != nil {
-		log.Fatal().Err(err).Msg("Worker initialization failed")
-	}
+	// Initialize the scheduler
+	scheduler := asynq.NewScheduler(redisConn, nil)
+
+	// Register cron jobs
+	scheduler.Register("0 * * * *", asynq.NewTask("type:cleanup", nil))
+
+	// Start the worker server
+	go func() {
+		if err := srv.Run(mux); err != nil {
+			log.Fatal().Err(err).Msg("Worker initialization failed")
+		}
+	}()
+
+	// Start the scheduler
+	go func() {
+		if err := scheduler.Run(); err != nil {
+			log.Fatal().Err(err).Msg("Scheduler initialization failed")
+		}
+	}()
+
+	log.Info().Msg("Worker and Scheduler are running...")
+
+	// Keep the server running unless interrupted
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+
+	// Graceful shutdown
+	scheduler.Shutdown()
+	srv.Shutdown()
+	log.Warn().Msg("Shutdown complete.")
 }
